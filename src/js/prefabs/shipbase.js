eb1fef5627df3869c7d6d4438e3876046c54292e
Patrick Yates <frozen.dinosaur@gmail.com>
2015-11-23 00:04:06 +1100

    Add more properties to ShipBase Also added collision functions

--- src/js/prefabs/shipbase.js
+++ src/js/prefabs/shipbase.js
@@ -1,6 +1,9 @@
-var ShipBase = function (game, x, y, key) {
+var ShipBase = function (game, x, y, key, weapon) {
 	//call base constructor
 	Phaser.Sprite.call(this, game, x, y, key);
+	
+	this.startX = x;
+	this.startY = y;
 	
 	//setup physics information
 	this.game.physics.arcade.enableBody(this);
@@ -9,11 +12,28 @@ var ShipBase = function (game, x, y, key) {
 	this.body.maxVelocity.set(400);
 	this.body.maxAngular = 200;
 	this.body.collideWorldBounds = true;
+	this.ACCELERATION = 600;
+	this.ANGULAR_ACCELERATION = 400;
+	
+	//set pivot point
+	this.anchor.setTo(0.5, 0.5);
+	
+	//Setup bullet timing information
+	this.lastBulletShotAt = -10000;
+	this.SHOT_DELAY = 100;
+	
+	//Add weapon
+	this.weapon = weapon;
+	weapon.parentShip = this;
 };
 
 ShipBase.prototype = Object.create(Phaser.Sprite.prototype);
 ShipBase.prototype.constructor = ShipBase;
 
-ShipBase.prototype.update = function () {
-	
+ShipBase.prototype.onHit = function (bullet) {
+	if (bullet.parentShip !== this) {this.reset(this.startX, this.startY);}
+};
+
+ShipBase.prototype.onShipCollision = function () {
+	this.reset(this.startX, this.startY);
 };
