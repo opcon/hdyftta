b730dbb0dbf715ddf2d27a62ea79fcb45053a4bc
Patrick Yates <frozen.dinosaur@gmail.com>
2015-11-23 00:05:06 +1100

    Made PlayerShip inherit from ShipBase object Moved most properties into ShipBase object Moved shooting logic into WeaponBase object

--- src/js/prefabs/playership.js
+++ src/js/prefabs/playership.js
@@ -1,30 +1,11 @@
-/* global Phaser, Bullet, messageState */
+/* global Phaser, messageState, ShipBase */
 var cursorKeys;
 var keyRotateClockwise;
 var keyRotateAntiClockwise;
 
-var PlayerShip = function (game, x, y, key) {
-	
+var PlayerShip = function (game, x, y, key, weapon) {	
 	//call base constructor
-	Phaser.Sprite.call(this, game, x, y, key);
-	
-	//create the bullet group
-	this.bulletGroup = this.game.add.group();
-	
-	//set pivot point
-	this.anchor.setTo(0.5, 0.5);
-	
-	//setup physics information
-	this.game.physics.arcade.enableBody(this);
-	this.body.drag.set(1000);
-	this.body.angularDrag = 1000;
-	this.body.maxVelocity.set(400);
-	this.body.maxAngular = 200;
-	this.body.collideWorldBounds = true;
-	
-	//Setup bullet timing information
-	this.lastBulletShotAt = -10000;
-	this.SHOT_DELAY = 100;
+	ShipBase.call(this, game, x, y, key, weapon);
 	
 	//setup input keys
 	cursorKeys = this.game.input.keyboard.createCursorKeys();
@@ -32,21 +13,17 @@ var PlayerShip = function (game, x, y, key) {
 	keyRotateAntiClockwise = this.game.input.keyboard.addKey(Phaser.Keyboard.Q);
 };
 
-PlayerShip.prototype = Object.create(Phaser.Sprite.prototype);
+PlayerShip.prototype = Object.create(ShipBase.prototype);
 PlayerShip.prototype.constructor = PlayerShip;
 
 PlayerShip.prototype.update = function () {
 	//handle bullet firing
-	if (this.game.time.now - this.lastBulletShotAt > this.SHOT_DELAY)
-	{
-		this.lastBulletShotAt = this.game.time.now;
-		this.fireBullet();
-	}
+	this.weapon.fire();
 	
 	if (keyRotateClockwise.isDown || messageState.cw) {
-		this.body.angularAcceleration = 400;
+		this.body.angularAcceleration = this.ANGULAR_ACCELERATION;
 	} else if (keyRotateAntiClockwise.isDown || messageState.ccw) {
-		this.body.angularAcceleration = -400;
+		this.body.angularAcceleration = -this.ANGULAR_ACCELERATION;
 	} else {
 		this.body.angularAcceleration = 0;
 	}
@@ -54,9 +31,9 @@ PlayerShip.prototype.update = function () {
 	var vertical = true;
 	var pv = new Phaser.Point(0, 0);
 	if (cursorKeys.up.isDown || messageState.up) {
-		pv = this.game.physics.arcade.accelerationFromRotation(this.rotation, 600);
+		pv = this.game.physics.arcade.accelerationFromRotation(this.rotation, this.ACCELERATION);
 	} else if (cursorKeys.down.isDown || messageState.down) {
-		pv = this.game.physics.arcade.accelerationFromRotation(this.rotation, -600);
+		pv = this.game.physics.arcade.accelerationFromRotation(this.rotation, -this.ACCELERATION);
 	} else {
 		vertical = false;
 	}
@@ -64,9 +41,9 @@ PlayerShip.prototype.update = function () {
 	var horizontal = true;
 	var ph = new Phaser.Point(0, 0);
 	if (cursorKeys.left.isDown || messageState.left) {
-		ph = this.game.physics.arcade.accelerationFromRotation(this.rotation + this.game.math.degToRad(90), -600);
+		ph = this.game.physics.arcade.accelerationFromRotation(this.rotation + this.game.math.degToRad(90), -this.ACCELERATION);
 	} else if (cursorKeys.right.isDown || messageState.right) {
-		ph = this.game.physics.arcade.accelerationFromRotation(this.rotation + this.game.math.degToRad(90), 600);
+		ph = this.game.physics.arcade.accelerationFromRotation(this.rotation + this.game.math.degToRad(90), this.ACCELERATION);
 	} else {
 		horizontal = false;
 	}
@@ -77,34 +54,4 @@ PlayerShip.prototype.update = function () {
 		var pFinal = Phaser.Point.add(ph, pv);
 		this.body.acceleration.set(pFinal.x, pFinal.y);
 	}
-};
-
-PlayerShip.prototype.fireBullet = function () {
-	//Get the first dead bullet
-	var bullet = this.bulletGroup.getFirstDead();
-	
-	//If there aren't any available, create a new one
-	if (bullet === null) {
-		//create a new bullet
-		bullet = new Bullet(this.game, 'bullet-1');
-		
-		//add bullet to bullet group
-		this.bulletGroup.add(bullet);
-		
-		//Enable physics for the bullet
-		//this.game.physics.arcade.enableBody(bullet);
-	}
-	
-	//We revive the selected missile
-	bullet.revive();
-	
-	//Enable checking for bullet outside of world bounds
-    bullet.checkWorldBounds = true;
-    bullet.outOfBoundsKill = true;
-	
-	//And move it to it's initial coordinates
-	bullet.reset(this.x, this.y);
-	
-	//Shoot it
-	bullet.fire(this.rotation);
 };
